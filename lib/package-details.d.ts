079e72940e1ed90b1a4f571f04ac7d935267c575
Saqib <saqibnoorani@gmail.com>
2020-05-26 14:47:08 +0530

    removing the build files in commit

--- lib/package-details.d.ts
+++
@@ -1,5 +0,0 @@
-declare var https: any;
-declare var npmjsURL: string;
-declare var npmsURL: string;
-declare function getQuickDetails(name: string, endDate: string, currentDate: string, callback: (arg0: any) => void): any;
-declare function getFullDetails(name: string, callback: (arg0: any) => void): any;
